2dda002d89a0d1b991cdb5b5e635c2ae98ad4f22
khmaladze-tiko <73258377+khmaladze-tiko@users.noreply.github.com>
2023-04-20 20:05:07 +0400

    backgroundColors

---
+++ script.js
@@ -0,0 +1,8 @@
+let index = 0;
+
+function changeColors() {
+    let colors = ["red", "blue", "yellow", "green", "purple", "orange"];
+
+    document.getElementsByTagName("body")[0].style.background = colors[index++];
+    if(index > colors.length - 1) index = 0;
+}
